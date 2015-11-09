9845bf8a420a91f56a90cfadf5b647645b049fd7
pointlook admin <admin@pointlook.com>
2015-11-09 12:38:50 +0000

    update-locale

--- scripts/installTools.sh
+++ scripts/installTools.sh
@@ -1,6 +1,10 @@
 apt-get update
-apt-get -y install screen mongodb nginx
+apt-get -y install screen mongodb nginx curl
 
 /usr/sbin/usermod -d /home/www -s /bin/bash www-data
 ( mkdir -p /home/www; cd /home; chown -R www-data: www )
+
+locale-gen en_US en_US.UTF-8
+dpkg-reconfigure locales
+
 
